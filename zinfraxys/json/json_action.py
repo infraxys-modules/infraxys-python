f1011fd07467f9b6c0ec4838d7ee40d747683523
Jeroen Manders <jeroen@manders.be>
2020-10-31 14:48:48 +0000

    Remove old files

--- zinfraxys/json/json_action.py
+++
@@ -1,4 +0,0 @@
-class JsonAction(object):
-
-    def __init__(self):
-        pass
--- zinfraxys/services/base_service.py
+++
@@ -1,7 +0,0 @@
-from infraxys.logger import Logger
-
-
-class BaseService(object):
-
-    def __init__(self):
-        self.logger = Logger.get_logger(self.__class__.__name__)
